5f550e8b3c0a26114bb304f37ec57fd30faef661
Vi-011 <vickyg11@nycstudents.net>
2025-05-12 10:04:08 -0400

    youknowwhereyouare

--- app.py
+++ app.py
@@ -5,31 +5,31 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # Get the first 150 Pokémon from the API
-    response = requests.get("https://pokeapi.co/api/v2/pokemon?limit=150")
+
+    response = requests.get("https://ghibliapi.vercel.app/species")
     data = response.json()
-    pokemon_list = data['results']
+    species_list = data['results']
     
-    # Create a list to hold Pokémon details
-    pokemons = []
+  
+    species = []
     
-    for pokemon in pokemon_list:
-        # Each Pokémon URL looks like "https://pokeapi.co/api/v2/pokemon/1/"
-        url = pokemon['url']
+    for thing in species_list:
+       
+        url = thing['url']
         parts = url.strip("/").split("/")
-        id = parts[-1]  # The last part is the Pokémon's ID
+        id = parts[-1]  
         
         # Create an image URL using the Pokémon's ID
         image_url = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png"
         
-        pokemons.append({
-            'name': pokemon['name'].capitalize(),
+        species.append({
+            'name': species['name'].capitalize(),
             'id': id,
             'image': image_url
         })
     
     # Send the Pokémon list to the index.html page
-    return render_template("index.html", pokemons=pokemons)
+    return render_template("index.html", species=species)
 
 # New route: When a user clicks a Pokémon card, this page shows more details and a stats chart
 @app.route("/pokemon/<int:id>")
@@ -40,8 +40,6 @@ def pokemon_detail(id):
     
     # Extract extra details like types, height, and weight
     types = [t['type']['name'] for t in data['types']]
-    height = data.get('height')
-    weight = data.get('weight')
     name = data.get('name').capitalize()
     image_url = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png"
     
@@ -55,11 +53,8 @@ def pokemon_detail(id):
         'id': id,
         'image': image_url,
         'types': types,
-        'height': height,
-        'weight': weight,
-        'stat_names': stat_names,
-        'stat_values': stat_values
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)j
+
