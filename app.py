9b1c8437fb375c3e1f51bd2a448c52bc6813ac9b
Vi-011 <vickyg11@nycstudents.net>
2025-05-13 10:15:55 -0400

    trhthfrth

--- app.py
+++ app.py
@@ -5,33 +5,62 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    my_dict = [{
+    "id": "b5a92d0e-5fb4-43d4-ba60-c012135958e4",
+    "name": "Spirit",
+    "classification": "Spirit",
+    "eye_colors": "Red",
+    "hair_colors": "Light Orange",
+    "url": "https://ghibliapi.vercel.app/species/b5a92d0e-5fb4-43d4-ba60-c012135958e4",
+    "people": [
+      "https://ghibliapi.vercel.app/people/ca568e87-4ce2-4afa-a6c5-51f4ae80a60b"
+    ],
+    "films": [
+      "https://ghibliapi.vercel.app/films/0440483e-ca0e-4120-8c50-4c8cd9b965d6"
+    ]
+  },
+  {
+    "id": "f25fa661-3073-414d-968a-ab062e3065f7",
+    "name": "God",
+    "classification": "God",
+    "eye_colors": "Brown",
+    "hair_colors": "White",
+    "url": "https://ghibliapi.vercel.app/species/f25fa661-3073-414d-968a-ab062e3065f7",
+    "people": [
+      "Moro"
+    ],
+    "films": [
+      "https://ghibliapi.vercel.app/films/0440483e-ca0e-4120-8c50-4c8cd9b965d6"
+    ]
+  }]
+    return my_dict
+    
 
-    response = requests.get("https://ghibliapi.vercel.app/species")
-    data = response.json()
-    species_list = data['results']
+response = requests.get("https://ghibliapi.vercel.app/species")
+data = response.json()
+species_list = data['results']
     
   
-    species = []
+species = []
     
-    for thing in species_list:
-       
-        url = thing['url']
-        parts = url.strip("/").split("/")
-        id = parts[-1]  
+for thing in species_list:
+    movie = image_url 
+    url = thing['url']
+    parts = url.strip("/").split("/")
+    id = parts[-1]  
         
         # Create an image URL using the Pokémon's ID
-        image_url = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png"
+    image_url = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png"
         
-        species.append({
-            'name': species['name'].capitalize(),
-            'id': id,
-            'image': image_url
+    species.append({
+        'name': species['name'].capitalize(),
+        'movie': movie,
+        'id': id,
+        'image': image_url
         })
     
-    # Send the Pokémon list to the index.html page
     return render_template("index.html", species=species)
 
-# New route: When a user clicks a Pokémon card, this page shows more details and a stats chart
 @app.route("/species/<int:id>")
 def species_detail(id):
     # Get detailed info for the Pokémon using its id
@@ -43,14 +72,14 @@ def species_detail(id):
     name = data.get('name').capitalize()
     image_url = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png"
     
-    # Extract base stats for the chart (e.g., hp, attack, defense, etc.)
+
     stat_names = [stat['stat']['name'] for stat in data['stats']]
     stat_values = [stat['base_stat'] for stat in data['stats']]
 
     return render_template("species.html", species={
         'name': name,
-        'id': id,
         'image': image_url,
+        'id': id,
         'types': types,
     })
 
